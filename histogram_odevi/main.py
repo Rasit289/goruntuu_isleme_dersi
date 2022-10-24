670d66a7640299357beccdeb8d7f47ead7786997
Rasit289 <82665438+Rasit289@users.noreply.github.com>
2022-10-24 15:36:57 +0300

    histogram_odevi
    bir resmin histogramını phyton  ile hesaplayan kod

---
+++ histogram_odevi/main.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from turtle import shape
+
+foto = cv2.imread("gul.jpg",0)
+
+
+s=foto.shape
+
+#dizimizi tanımloyoruz
+Hist = np.zeros(shape=(256,1))
+
+#dizide satır ve sütunları geziyoruz
+for v in range (s[0]):
+    for u in range (s[1]):
+        #dizideki her bir pikselin sayısal değerini buluyoruz
+        i= foto[v,u]
+        #historam dizimizi bir arttıyoruz.
+        Hist[i]=Hist[i]+1
+#dizimizin histogramının sayısal değerlerini yazdırıyoruz
+for t in range(256):
+    print(Hist [t])
+#histogramı gösteriyoruz
+plt.plot(Hist)
+plt.show()
+cv2.waitKey()
+
